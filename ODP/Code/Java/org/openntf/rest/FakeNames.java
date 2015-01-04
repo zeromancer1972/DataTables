f4e4f9442ef2e6361193b54dca1da13816e3b03d
zeromancer1972 <obusse@googlemail.com>
2015-01-04 05:24:51 +0100

    ZIP value

--- ODP/Code/Java/org/openntf/rest/FakeNames.java
+++ ODP/Code/Java/org/openntf/rest/FakeNames.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Vector;
 
 import org.openntf.domino.Document;
 import org.openntf.domino.DocumentCollection;
@@ -33,7 +34,7 @@ public class FakeNames implements Serializable {
 			collection.put("lastname", doc.getItemValueString("lastname"));
 			collection.put("firstname", doc.getItemValueString("firstname"));
 			collection.put("address", doc.getItemValueString("address"));
-			collection.put("zip", doc.getItemValue("zip", Integer.class).toString());
+			collection.put("zip", getZip(doc.getItemValue("zip")));
 			collection.put("city", doc.getItemValueString("city"));
 			collection.put("country", doc.getItemValueString("country"));
 
@@ -49,6 +50,16 @@ public class FakeNames implements Serializable {
 		} catch (IOException e) {
 
 			e.printStackTrace();
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private String getZip(final Vector v) {
+		if (v.elementAt(0).getClass() == String.class) {
+			return v.elementAt(0).toString();
+		} else {
+			return v.elementAt(0).toString().replace(".0", "");
 		}
 
 	}
